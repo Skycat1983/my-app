3203b225d4d1439d021ab6ea31dd2a1dabe7c0df
Skycat1983 <hlaoutaris@gmail.com>
2022-10-28 16:40:42 +0200

    ignore test

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Fetch from './Fetch'
 
 function Cards() {
   return (
-    <div>Cards</div>
+    <div>Cards
+      <Fetch />
+    </div>
   )
 }
 
---
+++ src/Components/Fetch.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Fetch() {
+
+ const fetchGames = async () => {
+
+ } 
+
+
+  return (
+    <div>Fetch</div>
+  )
+}
+
+export default Fetch
