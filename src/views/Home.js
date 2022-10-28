1a671fc423d02f8cdce04b9a9a24228ada365acf
Skycat1983 <hlaoutaris@gmail.com>
2022-10-28 15:55:06 +0200

    first update

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cards from '../Components/Cards'
+
+function Home() {
+  // console.log("we are in home");
+  return (
+    <div> 
+<h1>hi</h1>
+<Cards />
+    </div>
+  )
+}
+
+export default Home
+
+
+// MINIMUM VIABLE PRODUCT: user stories
+// 4 screens: list screen, details screen, login/register screen, chat room screen
