e01bee5809674fda3562cae9712d3742f250c1ad
Skycat1983 <hlaoutaris@gmail.com>
2022-10-29 16:05:27 +0200

    working on cards

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import Cards from '../Components/Cards'
+import Fetch from '../Components/Fetch'
 
 function Home() {
   // console.log("we are in home");
   return (
     <div> 
 <h1>hi</h1>
-<Cards />
+<Fetch />
     </div>
   )
 }
 
 export default Home
-
 
 // MINIMUM VIABLE PRODUCT: user stories
 // 4 screens: list screen, details screen, login/register screen, chat room screen
